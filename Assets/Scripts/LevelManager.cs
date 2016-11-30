c28843a6c947386be2fb387db2f81b66d9e11ece
ngocphong2608 <ttphong2608@gmail.com>
2016-11-30 11:03:04 +0700

    01.  Making a splash screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadingAfter;
+
+	void Start() {
+		Invoke ("LoadNextLevel", autoLoadingAfter);
+	}
+
+	public void LoadLevel(string name) {
+		//Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("I want to quit!");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel() {
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+
+	AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+		music = GetComponent<AudioSource> ();
+
+		if (instance != null) {
+			Destroy (gameObject);
+			//instance = null;
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+
+			music.clip = startClip;
+			music.loop = true;
+			music.Play ();
+		}
+	}
+
+	void OnLevelWasLoaded(int level) {
+		Debug.Log ("Level was loaded: " + level);
+		if (music == null)
+			return;
+
+		music.Stop ();
+
+		if (level == 0) {
+			music.clip = startClip;
+		} else if (level == 1) {
+			music.clip = gameClip;
+		} else if (level == 2) {
+			music.clip = endClip;
+		}
+
+		music.loop = true;
+		music.Play ();
+	}
+}
