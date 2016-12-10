8915fd0b764424041c26a211ce6e7b3db99a5b96
ngocphong2608 <ttphong2608@gmail.com>
2016-12-10 11:26:24 +0700

    29. Collision Matrix In Scripts

--- Assets/Scripts/Defenders.cs
+++ Assets/Scripts/Defenders.cs
@@ -1,20 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attackers : MonoBehaviour {
-
-	[Range (-1f, 1.5f)]
-	public float walkSpeed;
+public class Defenders : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Rigidbody2D myRig = gameObject.AddComponent<Rigidbody2D> ();
-		myRig.isKinematic = true;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
