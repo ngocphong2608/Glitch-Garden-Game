0ebaee24049675cea227f99298ae24d42f20d275
ngocphong2608 <ttphong2608@gmail.com>
2016-12-11 10:57:55 +0700

    32. Animating Defender Projectiles
    31. Using A Health Component

--- Assets/Scripts/Attackers.cs
+++ Assets/Scripts/Attackers.cs
@@ -5,11 +5,13 @@ public class Attackers : MonoBehaviour {
 	
 	float walkSpeed;
 	GameObject currentTarget;
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		Rigidbody2D myRig = gameObject.AddComponent<Rigidbody2D> ();
 		myRig.isKinematic = true;
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +20,7 @@ public class Attackers : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
-		Debug.Log (name + " triggered " + col.gameObject.name);
+		//Debug.Log (name + " triggered " + col.gameObject.name);
 	}
 
 	public void SetSpeed(float speed) {
@@ -26,7 +28,16 @@ public class Attackers : MonoBehaviour {
 	}
 
 	public void StrikeCurrentTarget(float damage) {
-		Debug.Log (name + " caused " + damage + " damage");
+		//Debug.Log (name + " caused " + damage + " damage");
+		if (currentTarget != null) {
+			Health h = currentTarget.GetComponent<Health> ();
+			bool isDead = h.DealingDamage(damage);
+			if (isDead) {
+				Destroy (currentTarget);
+				currentTarget = null;
+				anim.SetBool("IsAttacking", false);
+			}
+		}
 	}
 
 	public void Attack(GameObject obj) {
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float health = 50f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// return true if target is dead
+	public bool DealingDamage(float damage) {
+		health -= damage;
+		if (health <= 0)
+			return true;
+		return false;
+	}
+}
