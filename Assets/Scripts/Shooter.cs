7e6f7999cb442436459359e088a02f7509494afa
ngocphong2608 <ttphong2608@gmail.com>
2016-12-12 11:08:54 +0700

    37. Creating When Nedded

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,7 +3,16 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject projectile, projectileParent, gun;
+	public GameObject projectile, gun;
+
+	private GameObject projectileParent;
+
+	void Start() {
+		projectileParent = GameObject.Find ("Projectiles");
+		if (projectileParent == null) {
+			projectileParent = new GameObject("Projectiles");
+		}
+	}
 
 	public void FireGun() {
 		GameObject obj = Instantiate (projectile, gun.transform.position, Quaternion.identity) as GameObject;
