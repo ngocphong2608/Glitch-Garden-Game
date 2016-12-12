4c7d78ca79971349fcb379b1914d355c21bdf79c
ngocphong2608 <ttphong2608@gmail.com>
2016-12-12 10:48:19 +0700

    36. Tower Selector Buttons

---
+++ Assets/Scripts/ButtonPane.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPane : MonoBehaviour {
+
+	public GameObject defenderPref;
+	public static GameObject selectedDefender;
+
+	ButtonPane[] buttons;
+
+	// Use this for initialization
+	void Start () {
+		buttons = FindObjectsOfType<ButtonPane> ();
+		GetComponent<SpriteRenderer> ().color = Color.gray;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (selectedDefender);
+	}
+
+	void OnMouseDown() {
+		//Debug.Log (name + "pressed");
+		foreach (ButtonPane btn in buttons) {
+			btn.GetComponent<SpriteRenderer>().color = Color.gray;
+		}
+		GetComponent<SpriteRenderer> ().color = Color.white;
+		selectedDefender = gameObject;
+	}
+}
